24bf669c5732dc7eca2403e80392b648cc55144a
Football <whatsbuss1@gmail.com>
2026-02-19 09:05:52 +0000

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "Download TikTok Videos Without Watermark - ClipNova",
+  description:
+    "ClipNova is a free online TikTok video downloader. Download TikTok videos in HD without watermark. Fast, secure and unlimited.",
+  keywords:
+    "tiktok downloader, download tiktok video, no watermark, tiktok hd download, clipnova",
+};
